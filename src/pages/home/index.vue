1f928acb34be83703de4874bd23d26c920e08547
Warsono <warsono@gits.co.id>
2018-02-25 14:47:22 +0700

    update home content

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,18 +1,19 @@
 <template>
-    <v-layout justify-center align-center>
-        <v-tooltip right>
-        <v-btn
-            icon
-            large
-            href="#"
-            target="_blank"
-            slot="activator"
-        >
-            <v-icon large>code</v-icon>
-        </v-btn>
-        <span>Source</span>
-        </v-tooltip>
-    </v-layout>
+    <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
 
 <script>
