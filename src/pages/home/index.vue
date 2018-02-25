5aa17a82de04fbae0ebe9faea2fadbe326c04a24
Warsono <warsono@gits.co.id>
2018-02-25 14:01:04 +0700

    change homepage, removed HelloWorld component and added main layout component

---
+++ src/pages/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-layout justify-center align-center>
+        <v-tooltip right>
+        <v-btn
+            icon
+            large
+            href="#"
+            target="_blank"
+            slot="activator"
+        >
+            <v-icon large>code</v-icon>
+        </v-btn>
+        <span>Source</span>
+        </v-tooltip>
+    </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -13,8 +12,14 @@ export default new Router({
     },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: () => import('../layouts/main'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('../pages/home')
+        }
+      ]
     }
   ]
 })
