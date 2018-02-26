62ec07192e655681c5bf10ff1182a091b1f36ef0
Warsono <warsono@gits.co.id>
2018-02-26 14:35:07 +0700

    simplify route registration via route function

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import { route } from '../utils/route'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../pages/login')
-    },
-    {
-      path: '/',
-      component: () => import('../layouts/main'),
-      children: [
-        {
-          path: '/',
-          name: 'home',
-          component: () => import('../pages/home'),
-          meta: {
-            middleware: 'auth'
-          }
-        },
-        {
-          path: '/users',
-          name: 'users',
-          component: () => import('../pages/users'),
-          meta: {
-            middleware: 'auth'
-          }
-        }
-      ]
-    }
+    route('/login', 'login', 'pages/login'),
+    route('/', null, 'layouts/main', [
+      route('/', 'home', 'pages/home', null, 'auth'),
+      route('/users', 'users', 'pages/users', null, 'auth')
+    ])
   ]
 })
