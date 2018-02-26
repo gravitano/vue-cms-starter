cf59248adbcf5276d31dfce95956771e559fcca6
Warsono <warsono@gits.co.id>
2018-02-26 13:35:27 +0700

    update home page: added count card

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,22 +1,55 @@
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+      <v-layout row wrap>
+        <v-flex xs4 class="pr-4">
+          <v-card color="accent" class="white--text pa-2">
+            <v-card-title primary-title>
+              <v-layout row>
+                <v-flex xs6>
+                  <div class="display-2">108</div>
+                </v-flex>
+                <v-flex xs6>
+                  <div class="body-1 text-xs-right">Outlet</div>
+                </v-flex>
+              </v-layout>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs4 class="pr-4">
+          <v-card color="secondary" class="white--text pa-2">
+            <v-card-title primary-title>
+              <v-layout row>
+                <v-flex xs6>
+                  <div class="display-2">34</div>
+                </v-flex>
+                <v-flex xs6>
+                  <div class="body-1 text-xs-right">Salesman</div>
+                </v-flex>
+              </v-layout>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs4 class="pr-4">
+          <v-card color="error" class="white--text pa-2">
+            <v-card-title primary-title>
+              <v-layout row>
+                <v-flex xs6>
+                  <div class="display-2">215</div>
+                </v-flex>
+                <v-flex xs6>
+                  <div class="body-1 text-xs-right">Product</div>
+                </v-flex>
+              </v-layout>
+            </v-card-title>
+          </v-card>
+        </v-flex>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
 <script>
-export default {}
+  export default {}
 </script>
 
