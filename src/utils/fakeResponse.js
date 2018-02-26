2b9b140f589f7ca3b70f07657241877d97aa3174
Warsono <warsono@gits.co.id>
2018-02-26 15:00:40 +0700

    move user data to a const

--- src/utils/fakeResponse.js
+++ src/utils/fakeResponse.js
@@ -1,3 +1,11 @@
+const user = {
+  'id': 1,
+  'username': 'johndoe',
+  'email': 'johndoe@example.com',
+  'name': 'John Doe',
+  'avatar': null
+}
+
 export default {
   login: {
     data: {
@@ -5,25 +13,13 @@ export default {
       meta: {
         token: 'JOHNDOEDUMMYTOKEN123##'
       },
-      data: {
-        'id': 1,
-        'username': 'johndoe',
-        'email': 'johndoe@example.com',
-        'name': 'John Doe',
-        'avatar': null
-      }
+      data: user
     }
   },
   user: {
     data: {
       code: 200,
-      data: {
-        'id': 1,
-        'username': 'johndoe',
-        'email': 'johndoe@example.com',
-        'name': 'John Doe',
-        'avatar': null
-      }
+      data: user
     }
   }
 }
